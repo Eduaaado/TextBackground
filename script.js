df219da313bfd54f12f34684a2ce5582e84be39e
Eduardo Rodrigues <44787022+Eduaaado@users.noreply.github.com>
2019-02-16 09:10:47 -0200

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
   $(document).mousemove(function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
-    traX = ((4 * mouseX) / 570) + 40;
-    traY = ((4 * mouseY) / 570) + 50;
+    traX = (mouseX / 570) + 40;
+    traY = (mouseY / 570) / 570) + 50;
     console.log(traX);
     $(".title").css({"background-position": traX + "%" + traY + "%"});
   });
